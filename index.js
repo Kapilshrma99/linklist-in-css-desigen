118ac29b9a5362e816c9af92314bc25140aff323
Kapilshrma99 <96725122+Kapilshrma99@users.noreply.github.com>
2024-07-23 19:20:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const arr=[document.getElementById("a"),
+document.getElementById("b"),
+document.getElementById("c"),
+document.getElementById("d"),
+document.getElementById("e")]
+console.log();
+document.getElementById(arr[2].id).style.backgroundColor="red"
